a8464d0c1279534af4eea21f93dbdf37e3c6c410
fride02 <55950754+fride02@users.noreply.github.com>
2024-07-01 20:48:59 -0400

    Replaced Birch's bag with case
    Removed the original 3-starter bag and implemented the 9-starter case from the last merge. Very buggy and bad looking at the moment.

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -960,7 +960,7 @@ u8 GetSpecialBattleTransition(s32 id)
 
 void ChooseStarter(void)
 {
-    SetMainCallback2(CB2_ChooseStarter);
+    SetMainCallback2(CB2_GiveStarter);
     gMain.savedCallback = CB2_GiveStarter;
 }
 
--- src/ui_birch_case.c
+++ src/ui_birch_case.c
@@ -132,7 +132,7 @@ struct MonChoiceData{ // This is the format used to define a mon, everything lef
 //
 static const struct MonChoiceData sStarterChoices[9] = 
 {
-    [BALL_TOP_FIRST]        = {SPECIES_MUDKIP, 5, ITEM_POTION, BALL_NET, NATURE_JOLLY, 1, MON_MALE, {255, 255, 0, 0, 0, 0}, {31, 31, 31, 31, 31, 31}, {MOVE_FIRE_BLAST, MOVE_SHEER_COLD, MOVE_WATER_GUN, MOVE_THUNDER}, 0, 0, 0},
+    [BALL_TOP_FIRST]        = {SPECIES_MUDKIP, 5, ITEM_POTION, BALL_NET, NATURE_JOLLY, 2, MON_MALE, {255, 255, 0, 0, 0, 0}, {31, 31, 31, 31, 31, 31}, {MOVE_FIRE_BLAST, MOVE_SHEER_COLD, MOVE_WATER_GUN, MOVE_THUNDER}, 0, 0, 0},
     [BALL_TOP_SECOND]       = {SPECIES_TREECKO, 5},
     [BALL_MIDDLE_FIRST]     = {SPECIES_TORCHIC, 5},
 
