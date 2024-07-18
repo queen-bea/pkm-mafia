fc9c22e5f001682c240095d4b0db293dad7d3767
queen-bea <55950754+queen-bea@users.noreply.github.com>
2024-07-18 14:17:07 -0400

    Decreased shiny odds to 1/512
    Based on a chat gpt estimate that an average player will encounter ~900 wild pokemon during a typical playthrough

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -91,8 +91,8 @@
 #define DEFAULT_STAT_STAGE 6
 #define MAX_STAT_STAGE    12
 
-// Shiny odds - 1/64
-#define SHINY_ODDS 1024 // Actual probability is SHINY_ODDS/65536
+#define SHINY_ODDS 128 // Actual probability is SHINY_ODDS/65536
+                       //                     = 1/512
 
 // Ribbon IDs used by TV and Pokénav
 #define CHAMPION_RIBBON       0
